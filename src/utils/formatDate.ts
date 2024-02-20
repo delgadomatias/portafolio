f247b1d4c3991b8aebe1327a0e85ff985b77d329
Matias <mmatidelga2@gmail.com>
2024-02-19 21:56:10 -0300

    Update

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,22 +1,7 @@
 export function formatDate(date: Date): string {
-  const monthNames = [
-    "Enero",
-    "Febrero",
-    "Marzo",
-    "Abril",
-    "Mayo",
-    "Junio",
-    "Julio",
-    "Agosto",
-    "Septiembre",
-    "Octubre",
-    "Noviembre",
-    "Diciembre",
-  ];
-
-  const day = date.getDate().toString().padStart(2, "0");
-  const monthIndex = date.getMonth();
-  const year = date.getFullYear();
-
-  return `${day} de ${monthNames[monthIndex]} de ${year}`;
+  return Intl.DateTimeFormat("es-ES", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  }).format(date);
 }
