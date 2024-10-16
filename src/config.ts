de546200948577823e6245f6ffc908c5e2ff75ae
Matias <mmatidelga2@gmail.com>
2024-10-16 00:07:53 -0300

    styles: minor improvements

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,12 @@
 export const NAVBAR = {
   links: [
-    { href: "#home", title: "Inicio" },
-    { href: "#about-me", title: "Sobre mi" },
-    { href: "#projects", title: "Proyectos" },
-    { href: "#experience", title: "Experiencia" },
-    { href: "#blog", title: "Blog" },
+    {href: "#home", title: "Inicio"},
+    {href: "#about-me", title: "Sobre mi"},
+    {href: "#experience", title: "Experiencia"},
+    {href: "#projects", title: "Proyectos"},
   ],
 };
 
 export const SITE = {
   url: "https://www.matiasdelgado.com.ar",
-  blog: {
-    pagesPerSection: 3,
-  },
 };
